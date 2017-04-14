bb13f1c27cff9db8dec70407a2772c2802b36b05
MonicaAntal <monica.antal96@gmail.com>
2017-04-14 08:22:41 +0300

    Add files via upload

---
+++ TreeMap/src/treemap/Demo.java
@@ -0,0 +1,36 @@
+package treemap;
+import java.util.*;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+TreeMap<Edge, Integer> graf = new TreeMap<Edge,Integer>();
+		
+		
+		graf.put(new Edge(1,2),1);
+		graf.put(new Edge(2,3),2);
+		graf.put(new Edge(3,4),1);
+		graf.put(new Edge(4,5),7);
+		graf.put(new Edge(2,4),6);
+		
+		TreeSet<Point> set = new TreeSet<Point>(new PointComparator());
+		
+		set.add(new Point(1,2));
+		set.add(new Point(2,3));
+		set.add(new Point(6,4));
+		set.add(new Point(3,5));
+		
+		System.out.println(set);
+	
+		List<Integer> list = new ArrayList<Integer>(); 
+			list.addAll(graf.values());
+		Collections.sort(list);	
+		System.out.println(list);
+		System.out.println(graf);
+		
+		//to do : implement kruskal, prim
+
+	}
+
+}
---
+++ TreeMap/src/treemap/TreeComparator.java
@@ -0,0 +1,22 @@
+package treemap;
+import java.util.*;
+
+public class TreeComparator implements Comparator<Integer> {
+
+	public int compare(Integer o1, Integer o2) {
+		
+		return o1.compareTo(o2);
+		
+	}
+
+}
+
+class PointComparator implements Comparator<Point> {
+
+	public int compare(Point o1, Point o2) {
+		
+		return o1.x.compareTo(o2.x);
+		
+	}
+	
+}
