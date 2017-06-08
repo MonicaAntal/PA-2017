eba5925c3ef81d9ef0ab6731009b90c0576e7703
MonicaAntal <monica.antal96@gmail.com>
2017-06-09 01:43:04 +0300

    Create Heap.java

--- Huffman/src/huffman/Heap.java
+++ Huffman/src/huffman/Heap.java
@@ -1,78 +1,71 @@
 package huffman;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+public class Heap implements Comparator<Nod> {
 
-public class Heap{
-	ArrayList<Nod> tree;
-	
-	public Heap()
-	{
-		tree = new ArrayList<Nod>();
+	ArrayList<Nod> heap = new ArrayList<Nod>();
+
+	Heap() {
+		heap.add(0, null);
+	}
+
+	public int compare(Nod a, Nod b) {
+		if (a.getFrecventa() > b.getFrecventa())
+			return -1;
+		else if (a.getFrecventa() < b.getFrecventa())
+			return 1;
+		return 0;
+	}
+
+	public void Swap(int i, int j) {
+		Nod aux = heap.get(i);
+		heap.set(i, heap.get(j));
+		heap.set(j, aux);
+	}
+
+	public void sift_down(int i) {
+		int k, j;
+		k = i;
+		do {
+			j = k;
+			if (2 * j <= heap.size() - 1
+					&& compare(heap.get(2 * j), heap.get(k)) == 1)
+				k = 2 * j;
+			if (2 * j + 1 <= heap.size() - 1
+					&& compare(heap.get(2 * j + 1), heap.get(k)) == 1)
+				k = 2 * j + 1;
+			Swap(j, k);
+		} while (k != j);
+	}
+
+	public void make_heap() {
+		for (int i = (heap.size() - 1) / 2; i >= 1; i--)
+			sift_down(i);
+	}
+
+	public void insert(Nod value) {
+		heap.add(value);
+		sift_down(1);
+	}
+
+	public Nod findmin() {
+		return heap.get(1);
 	}
 	
-	void MakeHeap(){
-		
-		
-	}
-	
-	void Insert(){
-		
-		
-	}
-	
-	void SiftDown(int i){
-		
-		int k = i;
-		int j=k;
-		do
-		{
-			j=k;
-			if (2*j < tree.size() && tree.get(2*j).freq > tree.get(k).freq)
-			{
-				k = 2*j;
-			}
-			if (2*j+1 < tree.size() && tree.get(2*j+1).freq > tree.get(k).freq)
-			{
-				k = 2*j+1;
-			}
-			Swap(j,k);
+	public void delete_min() {
+		if (heap.size() == 2) {
+			heap.remove(heap.size() - 1);
+		} else {
+			heap.set(1, heap.get(heap.size() - 1));
+			heap.remove(heap.size() - 1);
+			sift_down(1);
 		}
-		while(j!=k);
-		
 	}
-	
-	static void Swap(Nod a , Nod b) 
-	{
-		Nod aux = a;
-		a = b;
-		b = aux;
-	}
-	void Swap(int i, int j) 
-	{
-		Nod aux = tree.get(i);
-		tree.set(i, tree.get(j));
-		tree.set(j, aux);
-		
-	}
-	
-	void Percolate(int k){
-		
-		int k = i;
-		int j;
-		do
-		{
-			j=k;
-			if(j>1 && tree.get(j/2).freq < tree.get(k).freq)
-			{
-				k=j/2;
-			}
-			Swap(j,k);
-			
+
+	public void afisare() {
+		for (int i = 1; i < heap.size(); i++) {
+			System.out.println(heap.get(i));
 		}
-		while(j!=k);  //nu cred ca functioneaza chestia asta
-		
-		
 	}
 }
